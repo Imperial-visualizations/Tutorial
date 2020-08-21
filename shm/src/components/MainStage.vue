9b8fe66c10a9ce705f88167bd3f04695e80addc4
Robert Jones <rdj4318@ic.ac.uk>
2020-08-21 11:44:01 +0100

    SHM animation working

--- shm/src/components/MainStage.vue
+++ shm/src/components/MainStage.vue
@@ -1,14 +1,22 @@
 <template>
-    <div id="parent" style="height: 100%; width:50%">
+    <div id="parent" style="height: 50%; width:50%; padding:50px;">
         <canvas id="canvas"></canvas>
     </div>
 </template>
 
 <script>
 export default {
+    props:{
+        mag: {
+            default: 0.005,
+        }
+    },
     mounted(){
+        let vm = this;
         let canvas = document.querySelector('canvas');
         let parent = document.getElementById('parent');
+        let x = 50 + canvas.width/2;
+        let dx = 0;
 
         canvas.width = parent.offsetWidth;
         canvas.height = parent.offsetHeight;
@@ -21,9 +29,28 @@ export default {
             c.clearRect(0,0,canvas.width, canvas.height);
 
             c.beginPath();
-            c.arc(canvas.width/2, canvas.height/2, 50, 0, Math.PI * 2); 
-            c.fillStyle = 'Orange';
+            c.moveTo(0,0);
+            c.lineTo(canvas.width, 0);
+            c.lineTo(canvas.width, canvas.height);
+            c.lineTo(0, canvas.height);
+            c.lineTo(0, 0);
+            c.strokeStyle = 'black';
+            c.stroke();
+
+            c.beginPath();
+            c.moveTo(x, canvas.height/2);
+            c.lineTo(canvas.width/2, canvas.height/2);
+            c.strokeStyle = 'blue';
+            c.lineWidth = 2;
+            c.stroke();
+
+            c.beginPath();
+            c.arc(x, canvas.height/2, 20, 0, Math.PI * 2); 
+            c.fillStyle = 'orange';
             c.fill();
+
+            x += dx;
+            dx += - vm.mag * (x-canvas.width/2);
         }
 
         animate();
