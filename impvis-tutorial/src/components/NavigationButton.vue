e7c0f5a2ba1bb50185847fa2c8166c7e2ca50b5b
xzhenghe <xzhenghe@gmail.com>
2020-11-25 10:19:36 +0000

    NavigationButton initial commit

---
+++ impvis-tutorial/src/components/NavigationButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div class="nav-group">
+            <iv-button class="nav-button">
+                <a
+                    :href="'./' + 'page_' + (pageNumber - 1) + '.html'"
+                    class="nav-button-text"
+                    >previous</a
+                >
+            </iv-button>
+            <iv-button class="nav-button">
+                <a
+                    :href="'./' + 'page_' + (pageNumber + 1) + '.html'"
+                    class="nav-button-text"
+                    >next</a
+                >
+            </iv-button>
+            <iv-pane-navigator />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pageName: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        pageNumber: function() {
+            console.log("pageName", this.pageName);
+            return Number(this.pageName.slice(4));
+        },
+    },
+};
+</script>
+<style>
+.nav-group {
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+}
+.nav-button {
+    margin-right: 20px;
+    margin-bottom: 20px;
+    background: #133f6f;
+}
+.nav-button-text {
+    color: white;
+}
+</style>
